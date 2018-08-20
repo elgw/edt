a81a187a0055785fd5f905a7bbb12a878bfe2985
erikw <erik.wernersson@scilifelab.se>
2018-08-20 13:43:29 +0200

    ...

--- eudist2.c
+++ eudist2.c
@@ -1,4 +1,6 @@
+#ifndef _POSIX_C_SOURCE
 #define _POSIX_C_SOURCE 199309L
+#endif
 
 #include <stdlib.h>
 #include <string.h>
@@ -12,14 +14,16 @@
 void edt2_brute_force(double * B, double * D, size_t M, size_t N)
 {
 
-  // Brute force O(n^2) implementation
-
+  /* Brute force O(n^2) implementation 
+   *
+   * This is used for testing correctness
+   *
+   * */
 
   for(int mm = 0; mm<M; mm++)
     for(int nn = 0; nn<N; nn++)
     {
-      double d_min = INFINITY;
-
+      double d_min = INFINITY; // Fallback, everything will be set to this if B is all zeros
       if(B[mm+M*nn] == 1)
       {
         D[mm+M*nn] = 0;
@@ -29,7 +33,6 @@ void edt2_brute_force(double * B, double * D, size_t M, size_t N)
         for(int kk = 0; kk<M; kk++)
           for(int ll = 0; ll<N; ll++)
           {
-
             if(B[kk+M*ll] == 1)
             {
               double d = pow(kk-mm,2)+pow(ll-nn,2);
@@ -39,13 +42,10 @@ void edt2_brute_force(double * B, double * D, size_t M, size_t N)
                   d_min = d;
                 }
             }
-
           }
-
         D[mm+M*nn] = d_min;
       }
     }
-
 
 }
 
@@ -84,81 +84,51 @@ void matrix_show(double * M, size_t m, size_t n)
 }
 
 
-void edt2(double * B, double * D, size_t M, size_t N)
+void pass12(double * B, double * D, size_t L)
 {
-  // Euclidean distance transform from objects in M, into D
-  // Matrices are of size M x N
-
-  size_t nL = max_size_t(M,N);
-  int bignum = 2*nL+1; // "infinity"
-
+  /* Pass 1 and 2 for a line (stride 1, since only run along the first dimension) */
+  
   // Pass 1
-  for(size_t kk = 0; kk<N; kk++) // For each column
+  double d = INFINITY;
+  for( size_t ll = 0; ll<L; ll++) // For each row
   {
-    double d = bignum;
-    for( size_t ll = 0; ll<M; ll++) // For each row
-    {
-      size_t pos = kk*M+ll;
-      d++;
-      if(B[pos] == 1)
-        d = 0;
-      D[pos] = d;
-    }
-  }
-  if(verbose>1)
-  {
-  printf("D1:\n");
-  matrix_show(D, M, N);
+    d++;
+    if(B[ll] == 1)
+      d = 0;
+    D[ll] = d;
   }
 
   // Pass 2
-  for(size_t kk = N ; kk-- > 0 ; ) // For each column
+  d = INFINITY;
+  for( size_t ll = L ; ll-- > 0; ) // For each row
   {
-    double d = bignum;
-    for( size_t ll = M ; ll-- > 0; ) // For each row
-    {
-      size_t pos = kk*M+ll;
-      d++;
-      if(B[pos] == 1)
-        d = 0;
-      if(d<D[pos])
-        D[pos] = d;
-    }
+    d++;
+    if(B[ll] == 1)
+      d = 0;
+    if(d<D[ll])
+      D[ll] = d;
   }
 
+}
 
-  int * S = malloc(nL*sizeof(double));
-  int * T = malloc(nL*sizeof(double));
-  if(verbose>1)
-  {
-  printf("D2:\n");
-  matrix_show(D, M, N);
-  }
-
-  //maintain only one line, not all of this
-  double * D0 = malloc(M*N*sizeof(double));
-  memcpy(D0, D, M*N*sizeof(double));
-
-
-  // Pass 3 and 4
-  for(int ll = 0; ll<M; ll++) // Select row
-  {
+void pass34(double * D, double * D0, int * S, double * T, int L, int stride)
+{
     // 3: Forward
     int q = 0;
-    int w = 0;
+    double w = 0;
     S[0] = 0;
     T[0] = 0;
 
-    for(int kk = 1; kk<N; kk++) // For each column
+    for(int kk = 1; kk<L; kk++) // For each column
     {
       // f(t[q],s[q]) > f(t[q], u)
       // f(x,i) = (x-i)^2 + g(i)^2
-      while(q>=0 && ((pow(T[q]-S[q],2) + pow(D[M*S[q]+ll],2)) > (pow(T[q]-kk, 2) + pow(D[M*kk+ll],2))))
+      while(q>=0 && ((pow(T[q]-S[q],2) + pow(D[stride*S[q]],2)) > (pow(T[q]-kk, 2) + pow(D[stride*kk],2))))
         q--;
 
       if(q<0)
       {
-        if(verbose > 1)
+        if(verbose > 2)
           printf("reset\n");
         q = 0;
         S[0] = kk;
@@ -168,11 +138,11 @@ void edt2(double * B, double * D, size_t M, size_t N)
         // w = 1 + Sep(s[q],u)
         // Sep(i,u) = (u^2-i^2+g(u)^2-g(i)^2) div (2(u-i))
         w = 1 + floor0((pow(kk,2)-pow(S[q],2) 
-              + pow(D[M*kk+ll],2) - pow(D[M*S[q]+ll],2))/(2*(kk-S[q])));
-        if(verbose > 1)
-          printf("u/kk: %d, S[q] = %d, q: %d w: %d\n", kk, S[q], q, w);
+              + pow(D[stride*kk],2) - pow(D[stride*S[q]],2))/(2*(kk-S[q])));
+        if(verbose > 2)
+          printf("u/kk: %d, S[q] = %d, q: %d w: %f\n", kk, S[q], q, w);
 
-        if(w<(int) N)
+        if(w<(int) L)
         {
           q++;
           S[q] = kk;
@@ -183,15 +153,62 @@ void edt2(double * B, double * D, size_t M, size_t N)
     }
 
     // 4: Backward  
-    for(int kk = N-1; kk > -1 ; kk--)
+    for(int kk = L-1; kk > -1 ; kk--)
     {
       //dt[u,y]:=f(u,s[q])
       if(verbose>1)
         printf("kk: %d, q: %d S[%d] = %d\n", kk, q, q, S[q]);
-      D[kk*M+ll] = pow(kk-S[q],2)+pow(D0[M*S[q]+ll], 2);
+      D[kk*stride] = pow(kk-S[q],2)+pow(D0[stride*S[q]], 2);
       if(kk == T[q])
         q--;
     }
+}
+
+
+void edt2(double * B, double * D, size_t M, size_t N)
+{
+  // Euclidean distance transform from objects in M, into D
+  // Matrices are of size M x N
+
+  size_t nL = max_size_t(M,N);
+
+  // 
+  // First dimension, pass 1 and 2
+  //
+  
+  for(size_t kk = 0; kk<N; kk++) // For each column
+  {
+    pass12(B+kk*M, D+kk*M, M);
+  }
+
+  if(verbose>1)
+  {
+    printf("D:\n");
+    matrix_show(D, M, N);
+  }
+
+  int * S = malloc(nL*sizeof(double));
+  double * T = malloc(nL*sizeof(double));
+
+  if(verbose>1)
+  {
+    printf("D2:\n");
+    matrix_show(D, M, N);
+  }
+
+  //maintain only one line, not all of this
+  double * D0 = malloc(M*N*sizeof(double));
+  memcpy(D0, D, M*N*sizeof(double));
+
+  // 
+  // Second dimension, Pass 3 and 4
+  //
+  
+  for(int ll = 0; ll<M; ll++) // Select row
+  {
+    int length = N;
+    int stride = M;
+    pass34(D+ll, D0+ll, S, T, length, stride);
   }
 
   free(D0);
@@ -204,8 +221,8 @@ void edt2(double * B, double * D, size_t M, size_t N)
 int main(int argc, char ** argv)
 {
 
-  size_t M = 1024;
-  size_t N = 1024;
+  size_t M = 5;
+  size_t N = 7;
 
   printf("Problem size: %zu x %zu\n", M, N);
 
@@ -224,14 +241,14 @@ int main(int argc, char ** argv)
   printf("Edt^2:\n");
   clock_gettime(CLOCK_MONOTONIC, &start0);
   for(int kk = 0 ; kk<1000; kk++)
-  edt2(B, D, M, N);
+    edt2(B, D, M, N);
   clock_gettime(CLOCK_MONOTONIC, &end0);
 
   matrix_show(D, M, N);
-int has_ref = 0;
+  int has_ref = 0;
   printf("Edt^2 -- brute force reference:\n");
   clock_gettime(CLOCK_MONOTONIC, &start1);
-if(M < 100 && N<100)
+  if(M < 100 && N<100)
   {
     edt2_brute_force(B, D_bf, M, N);
     has_ref = 1;
@@ -246,15 +263,14 @@ if(M < 100 && N<100)
   elapsed1 = (end1.tv_sec - start1.tv_sec);
   elapsed1 += (end1.tv_nsec - start1.tv_nsec) / 1000000000.0;
 
-
   matrix_show(D_bf, M, N);
 
   if(has_ref == 1)
   {
-  for(size_t kk = 0; kk<M*N; kk++)
-    assert(D[kk] == D_bf[kk]);
+    for(size_t kk = 0; kk<M*N; kk++)
+      assert(D[kk] == D_bf[kk]);
 
-  printf("Correct result\n");
+    printf("Correct result\n");
   }
   else 
   {
--- main.m
+++ main.m
@@ -1,10 +1,74 @@
-M = zeros(1024,1024);
-M(3,1) = 1;
-M(12,1) = 1;
+function main()
 
+compile()
 
-tic
-for kk = 1:1000
-    D = bwdist(M);
+test_correct_2D()
+
+test_speed()
+
 end
-toc
+
+function test_correct_2D()
+
+for kk = 1:1000
+    M = zeros(randi(100)+10, randi(100)+10);
+    M(randi(size(M,1)), randi(size(M,2))) = 1;
+    D1 = df_eudist2(M);
+    D2 = bwdist(M);
+       
+    err = max(abs(D1(:).^(1/2)-D2(:)));
+    assert(err<10e-3);          
+end
+
+fprintf('%d 2D tests ok!\n', kk);
+
+end
+
+function compile()
+    mex CFLAGS='$CFLAGS -std=c99 -march=native' COPTIMFLAGS='-O3 -DNDEBUG' df_eudist2.c
+end
+
+
+function test_speed()
+
+N = linspace(1,10e6, 50);
+N = round(sqrt(N));
+K = 2; % number of trials
+t_matlab = zeros(numel(N),1);
+t_df = zeros(numel(N),1);
+
+for nn = 1:numel(N)
+    progressbar(nn, numel(N));
+    B = zeros(N(nn),N(nn));
+    for kk = 1:10
+        B(randi(size(B,1)), randi(size(B,2))) = 1;
+    end
+    D = zeros(size(B));    
+    tic
+    for kk = 1:K
+    D = bwdist(B);
+    end
+    t_matlab(nn) = toc/K;
+    
+    tic
+    for kk = 1:K
+    D = df_eudist2(B);
+    end
+    
+    t_df(nn) = toc/K;    
+end
+
+figure
+plot(N.^2, t_matlab, 'k')
+hold on
+plot(N.^2, t_df, 'r');
+legend({'Matlab/bwdist', 'df eudist'});
+
+plot(N.^2, t_matlab, 'ko')
+plot(N.^2, t_df, 'ro');
+
+
+xlabel('Number of voxels')
+ylabel('Time (s)');
+
+end
